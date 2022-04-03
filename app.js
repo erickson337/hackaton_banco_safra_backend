fe33b65db6203c1c17b530b363af9e60f1a71a9f
erickson <ericksonicodemos@outlook.com>
2022-04-03 15:48:58 -0300

    Enable cors for all origin

--- app.js
+++ app.js
@@ -2,15 +2,17 @@ const express = require('express')
 const app = express()
 const bodyParser = require('body-parser')
 const routes = require('./routes')
+const cors = require('cors')
 
 require('dotenv').config()
 
 app.use(bodyParser.urlencoded({ extended: true }))
 app.use(bodyParser.json())
+app.use(cors())
 
 const port = process.env.PORT || 8080
 
 app.use('/api', routes)
 app.listen(port)
 
-console.log(`Running on http://localhost:${port}`);
+console.log(`Running on http://localhost:${port}`)
