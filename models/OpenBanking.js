fe8c3da276825389b457da4226424122358caa85
erickson <ericksonicodemos@outlook.com>
2022-04-03 17:56:50 -0300

    Improvement performance endpoint of openbanking

--- models/OpenBanking.js
+++ models/OpenBanking.js
@@ -1,9 +1,33 @@
 class OpenBanking {
-  constructor() {
+  constructor(nameBank, isProductBusiness, nameProduct, minValue, maxValue, nameBankOtherBank, minValueOtherBank, maxValueOtherBank, safraMaxValueIsBetter, safraMinValueIsBetter) {
+    this.nameBank = nameBank
+    this.isProductBusiness = isProductBusiness
+    this.nameProduct = nameProduct
+    this.minValue = minValue
+    this.maxValue = maxValue
+    this.nameBankOtherBank = nameBankOtherBank
+    this.minValueOtherBank = minValueOtherBank
+    this.maxValueOtherBank = maxValueOtherBank
+    this.safraMaxValueIsBetter = safraMaxValueIsBetter
+    this.safraMinValueIsBetter = safraMinValueIsBetter
   }
 
   toDTO() {
     return {
+      nameBank: this.nameBank,
+      isProductBusiness: this.isProductBusiness,
+      nameProduct: this.nameProduct,
+      minValue: this.minValue,
+      maxValue: this.maxValue,
+      nameBankOtherBank: this.nameBankOtherBank,
+      minValueOtherBank: this.minValueOtherBank,
+      maxValueOtherBank: this.maxValueOtherBank,
+      safraMaxValueIsBetter: this.safraMaxValueIsBetter,
+      safraMinValueIsBetter: this.safraMinValueIsBetter
     }
   }
+}
+
+module.exports = {
+  OpenBanking
 }
--- service/OpenBankingService.js
+++ service/OpenBankingService.js
@@ -1,4 +1,5 @@
 const axios = require('axios')
+const { OpenBanking } = require('../models')
 
 const find = async () => {
   const URL_COMPARATOR = [
@@ -9,41 +10,45 @@ const find = async () => {
     ['https://api.safra.com.br/open-banking', 'https://openbanking.api.santander.com.br/open-banking'],
     ['https://api.safra.com.br/open-banking', 'https://openbanking.api.santander.com.br/open-banking'],
   ]
-  return [
-    await compareServiceProductWithOtherBank(URL_COMPARATOR[0], '/products-services/v1/personal-credit-cards', 'personalCreditCards', 'UTILIZACAO_CANAIS_ATENDIMENTO_RETIRADA_ESPECIE_BRASIL'),
-    await compareServiceProductWithOtherBank(URL_COMPARATOR[1], '/products-services/v1/personal-accounts', 'personalAccounts', 'TRANSFERENCIA_DOC_TERMINAL_AUTOATENDIMENTO_OUTROS_MEIOS_ELETRONICOS'),
-    await compareServiceProductWithOtherBank(URL_COMPARATOR[2], '/products-services/v1/business-credit-cards', 'businessCreditCards', 'UTILIZACAO_CANAIS_ATENDIMENTO_RETIRADA_ESPECIE_BRASIL'),
-    await compareServiceProductWithOtherBank(URL_COMPARATOR[3], '/products-services/v1/personal-unarranged-account-overdraft', 'personalUnarrangedAccountOverdraft', 'CONCESSAO_ADIANTAMENTO_DEPOSITANTE'),
-    await compareServiceProductWithOtherBank(URL_COMPARATOR[4], '/products-services/v1/business-unarranged-account-overdraft', 'businessUnarrangedAccountOverdraft', 'CONCESSAO_ADIANTAMENTO_DEPOSITANTE'),
-    await compareServiceProductWithOtherBank(URL_COMPARATOR[5], '/products-services/v1/personal-accounts', 'personalAccounts', 'TRANSFERENCIA_TED_TERMINAL_AUTOATENDIMENTO_OUTROS_MEIOS_ELETRONICOS'),
-
+  const requestPromises = [
+    compareServiceProductWithOtherBank(URL_COMPARATOR[0], '/products-services/v1/personal-credit-cards', 'personalCreditCards', 'UTILIZACAO_CANAIS_ATENDIMENTO_RETIRADA_ESPECIE_BRASIL'),
+    compareServiceProductWithOtherBank(URL_COMPARATOR[1], '/products-services/v1/personal-accounts', 'personalAccounts', 'TRANSFERENCIA_DOC_TERMINAL_AUTOATENDIMENTO_OUTROS_MEIOS_ELETRONICOS'),
+    compareServiceProductWithOtherBank(URL_COMPARATOR[2], '/products-services/v1/business-credit-cards', 'businessCreditCards', 'UTILIZACAO_CANAIS_ATENDIMENTO_RETIRADA_ESPECIE_BRASIL'),
+    compareServiceProductWithOtherBank(URL_COMPARATOR[3], '/products-services/v1/personal-unarranged-account-overdraft', 'personalUnarrangedAccountOverdraft', 'CONCESSAO_ADIANTAMENTO_DEPOSITANTE'),
+    compareServiceProductWithOtherBank(URL_COMPARATOR[4], '/products-services/v1/business-unarranged-account-overdraft', 'businessUnarrangedAccountOverdraft', 'CONCESSAO_ADIANTAMENTO_DEPOSITANTE'),
+    compareServiceProductWithOtherBank(URL_COMPARATOR[5], '/products-services/v1/personal-accounts', 'personalAccounts', 'TRANSFERENCIA_TED_TERMINAL_AUTOATENDIMENTO_OUTROS_MEIOS_ELETRONICOS')
   ]
+
+  return Promise.all(requestPromises).then(values => values);
 }
 
 const compareServiceProductWithOtherBank = async (endpoints = [], path, namePropApi, nameProduct) => {
-  const getPropsFunction = getPropertiesApi(namePropApi, nameProduct);
+  const getDataModified = getPropertiesApi(namePropApi, nameProduct);
   const resultSafra = await axios.get(`${endpoints[0]}${path}`).then(({ data }) => data)
   const resultOtherBank = await axios.get(`${endpoints[1]}${path}`).then(({ data }) => data)
 
-  return getPropsFunction(resultSafra, resultOtherBank)
+  return getDataModified(resultSafra, resultOtherBank)
 }
 
 const getPropertiesApi = (namePropApi, nameProduct) => {
   return ({ data: dataSafra }, { data: dataOtherBank }) => {
-    const resultSafra = getPropertyOfProduct(dataSafra, namePropApi, nameProduct)
-    const resultOtherBank = getPropertyOfProduct(dataOtherBank, namePropApi, nameProduct)
+    const FAIL_SEARCH_PRODUCT = 'N/A';
+    const resultSafra = getPropertyOfProduct(dataSafra, namePropApi, nameProduct) ?? {}
+    const resultOtherBank = getPropertyOfProduct(dataOtherBank, namePropApi, nameProduct) ?? {}
+    const isProductBusiness = namePropApi.indexOf('business') >= 0 ? true : false
 
-    return {
-      nameBank: dataSafra.brand.name,
-      nameProduct: resultSafra.name,
-      minValue: resultSafra.minimum.value,
-      maxValue: resultSafra.maximum.value,
-      nameBankOtherBank: 'Banco B',
-      minValueOtherBank: resultOtherBank.minimum.value,
-      maxValueOtherBank: resultOtherBank.maximum.value,
-      safraMaxValueIsBetter: parseFloat(resultSafra.minimum.value) <= parseFloat(resultOtherBank.minimum.value) ? true : false,
-      safraMinValueIsBetter: parseFloat(resultSafra.maximum.value) <= parseFloat(resultOtherBank.maximum.value) ? true : false
-    }
+    return new OpenBanking(
+      dataSafra.brand.name ?? FAIL_SEARCH_PRODUCT,
+      isProductBusiness,
+      resultSafra.name ?? FAIL_SEARCH_PRODUCT,
+      resultSafra.minimum.value ?? FAIL_SEARCH_PRODUCT,
+      resultSafra.maximum.value ?? FAIL_SEARCH_PRODUCT,
+      'Banco B',
+      resultOtherBank.minimum.value ?? FAIL_SEARCH_PRODUCT,
+      resultOtherBank.maximum.value ?? FAIL_SEARCH_PRODUCT,
+      parseFloat(resultSafra.minimum?.value) <= parseFloat(resultOtherBank.minimum?.value) ? true : false,
+      parseFloat(resultSafra.maximum?.value) <= parseFloat(resultOtherBank.maximum?.value) ? true : false
+    ).toDTO();
   }
 }
 
@@ -51,18 +56,13 @@ const getPropertyOfProduct = (data, namePropApi, nameProduct) => {
   const { brand: { companies } } = data
   const firstCompany = companies[0]
 
-  if (firstCompany[namePropApi].length === 1) {
-    for (let service of firstCompany[namePropApi]) {
-      return filterProduct(service, nameProduct)
-    }
-  } else {
-    for (let key in firstCompany[namePropApi]) {
-      let result = filterProduct(firstCompany[namePropApi][key], nameProduct)
-      if (result) {
-        return result
-      }
+  for (let service of firstCompany[namePropApi]) {
+    let result = filterProduct(service, nameProduct)
+    if (result) {
+      return result
     }
   }
+  return null
 }
 
 const filterProduct = (company, nameProduct) => {
