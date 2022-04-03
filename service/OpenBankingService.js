fc55bf0710c842857dca7a53526455125a40c2a0
erickson <ericksonicodemos@outlook.com>
2022-04-03 03:36:25 -0300

    Add new url's and improvement function of get product

--- service/OpenBankingService.js
+++ service/OpenBankingService.js
@@ -2,10 +2,21 @@ const axios = require('axios')
 
 const find = async () => {
   const URL_COMPARATOR = [
-    ['https://api.safra.com.br/open-banking', 'https://openbanking.api.santander.com.br/open-banking']
+    ['https://api.safra.com.br/open-banking', 'https://openbanking.api.santander.com.br/open-banking'],
+    ['https://api.safra.com.br/open-banking', 'https://openbanking.api.santander.com.br/open-banking'],
+    ['https://api.safra.com.br/open-banking', 'https://openbanking.api.santander.com.br/open-banking'],
+    ['https://api.safra.com.br/open-banking', 'https://openbanking.api.santander.com.br/open-banking'],
+    ['https://api.safra.com.br/open-banking', 'https://openbanking.api.santander.com.br/open-banking'],
+    ['https://api.safra.com.br/open-banking', 'https://openbanking.api.santander.com.br/open-banking'],
   ]
   return [
-    await compareServiceProductWithOtherBank(URL_COMPARATOR[0], '/products-services/v1/personal-credit-cards', 'personalCreditCards', 'UTILIZACAO_CANAIS_ATENDIMENTO_RETIRADA_ESPECIE_BRASIL')
+    await compareServiceProductWithOtherBank(URL_COMPARATOR[0], '/products-services/v1/personal-credit-cards', 'personalCreditCards', 'UTILIZACAO_CANAIS_ATENDIMENTO_RETIRADA_ESPECIE_BRASIL'),
+    await compareServiceProductWithOtherBank(URL_COMPARATOR[1], '/products-services/v1/personal-accounts', 'personalAccounts', 'TRANSFERENCIA_DOC_TERMINAL_AUTOATENDIMENTO_OUTROS_MEIOS_ELETRONICOS'),
+    await compareServiceProductWithOtherBank(URL_COMPARATOR[2], '/products-services/v1/business-credit-cards', 'businessCreditCards', 'UTILIZACAO_CANAIS_ATENDIMENTO_RETIRADA_ESPECIE_BRASIL'),
+    await compareServiceProductWithOtherBank(URL_COMPARATOR[3], '/products-services/v1/personal-unarranged-account-overdraft', 'personalUnarrangedAccountOverdraft', 'CONCESSAO_ADIANTAMENTO_DEPOSITANTE'),
+    await compareServiceProductWithOtherBank(URL_COMPARATOR[4], '/products-services/v1/business-unarranged-account-overdraft', 'businessUnarrangedAccountOverdraft', 'CONCESSAO_ADIANTAMENTO_DEPOSITANTE'),
+    await compareServiceProductWithOtherBank(URL_COMPARATOR[5], '/products-services/v1/personal-accounts', 'personalAccounts', 'TRANSFERENCIA_TED_TERMINAL_AUTOATENDIMENTO_OUTROS_MEIOS_ELETRONICOS'),
+
   ]
 }
 
@@ -24,15 +35,14 @@ const getPropertiesApi = (namePropApi, nameProduct) => {
 
     return {
       nameBank: dataSafra.brand.name,
-      nameProduct: resultSafra[0].name,
-      minValue: resultSafra[0].minimum.value,
-      maxValue: resultSafra[0].maximum.value,
+      nameProduct: resultSafra.name,
+      minValue: resultSafra.minimum.value,
+      maxValue: resultSafra.maximum.value,
       nameBankOtherBank: 'Banco B',
-      nameProductOtherBank: resultOtherBank[0].name,
-      minValueOtherBank: resultOtherBank[0].minimum.value,
-      maxValueOtherBank: resultOtherBank[0].maximum.value,
-      valueDiffSafraMin: resultSafra[0].minimum.value > resultOtherBank[0].minimum.value ? true : false,
-      valueDiffSafraMax: resultSafra[0].maximum.value > resultOtherBank[0].maximum.value ? true : false
+      minValueOtherBank: resultOtherBank.minimum.value,
+      maxValueOtherBank: resultOtherBank.maximum.value,
+      safraMaxValueIsBetter: parseFloat(resultSafra.minimum.value) <= parseFloat(resultOtherBank.minimum.value) ? true : false,
+      safraMinValueIsBetter: parseFloat(resultSafra.maximum.value) <= parseFloat(resultOtherBank.maximum.value) ? true : false
     }
   }
 }
@@ -40,7 +50,24 @@ const getPropertiesApi = (namePropApi, nameProduct) => {
 const getPropertyOfProduct = (data, namePropApi, nameProduct) => {
   const { brand: { companies } } = data
   const firstCompany = companies[0]
-  return firstCompany[namePropApi].map(personalCreditCard => personalCreditCard.fees['services'] && personalCreditCard.fees['services'].find(field => field.name === nameProduct))
+
+  if (firstCompany[namePropApi].length === 1) {
+    for (let service of firstCompany[namePropApi]) {
+      return filterProduct(service, nameProduct)
+    }
+  } else {
+    for (let key in firstCompany[namePropApi]) {
+      let result = filterProduct(firstCompany[namePropApi][key], nameProduct)
+      if (result) {
+        return result
+      }
+    }
+  }
+}
+
+const filterProduct = (company, nameProduct) => {
+  const propService = company.fees['services'] ? company.fees['services'] : company.fees['priorityServices']
+  return propService && propService.find(field => field.name === nameProduct)
 }
 
 module.exports = {
